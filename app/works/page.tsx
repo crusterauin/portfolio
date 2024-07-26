70b3bcc0298f4afcdfc9b7ac70427e03405cb668
crusterauin <22106050012@student.uin-suka.ac.id>
2024-07-26 20:56:17 +0700

    responsive design at works

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -12,9 +12,9 @@ export default function Works() {
         <main className="min-h-full relative">
             <section className="flex flex-col items-center">
                 <div className="w-full max-w-5xl flex flex-row justify-center items-center gap-7 my-7">
-                    <h1 className="text-black text-sm font-regular font-['Plus Jakarta Sans']" onClick={() => setCategory("Mobile Apps")}>Mobile Apps</h1>
-                    <h1 className="text-black text-sm font-regular font-['Plus Jakarta Sans']" onClick={() => setCategory("Web Apps")}>Web Apps</h1>
-                    <h1 className="text-black text-sm font-regular font-['Plus Jakarta Sans']" onClick={() => setCategory("Graphic Design")}>Graphic Design</h1>
+                    <h1 className="text-black text-sm sm:text-lg lg:text-2xl font-regular font-['Plus Jakarta Sans']" onClick={() => setCategory("Mobile Apps")}>Mobile Apps</h1>
+                    <h1 className="text-black text-sm sm:text-lg lg:text-2xl font-regular font-['Plus Jakarta Sans']" onClick={() => setCategory("Web Apps")}>Web Apps</h1>
+                    <h1 className="text-black text-sm sm:text-lg lg:text-2xl font-regular font-['Plus Jakarta Sans']" onClick={() => setCategory("Graphic Design")}>Graphic Design</h1>
                 </div>
                 <div className="w-full max-w-5xl flex flex-col items-center">
                     {category === 'Mobile Apps' && <Android />}
--- components/AndroidCard/index.tsx
+++ components/AndroidCard/index.tsx
@@ -1,11 +1,11 @@
 export default function Android() {
     return (
-        <div className="w-72 h-fit flex flex-col items-center gap-5 mb-10">
-            <div className="w-full h-32 flex flex-row">
+        <div className="w-72 sm:w-120 lg:w-[50rem] h-fit flex flex-col items-center gap-5 mb-10">
+            <div className="w-full h-32 sm:h-56 lg:h-96 flex flex-row">
                 <div className="w-1/3 bg-white border relative">
                     <div className="w-full absolute bottom-0 p-2">
-                        <h1 className="text-black text-xs font-regular font-['Plus Jakarta Sans']">My Anime List</h1>
-                        <h1 className="text-black text-xs font-thin font-['Plus Jakarta Sans']">Android</h1>
+                        <h1 className="text-black text-xs sm:text-sm lg:text-lg font-regular font-['Plus Jakarta Sans']">My Anime List</h1>
+                        <h1 className="text-black text-xs sm:text-sm lg:text-lg font-thin font-['Plus Jakarta Sans']">Android</h1>
                     </div>
                 </div>
                 <div className="w-2/3 bg-gray-700"></div>
